3460f77351ddec6b2834e53c71a527bb8e3a4cfb
erictatchell <erictatch@gmail.com>
2024-09-25 12:06:57 -0700

    unit tests for DCT

--- dct.go
+++ dct.go
@@ -1,6 +1,8 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 func C(x int) float64 {
 	if x == 0 {
@@ -13,15 +15,15 @@ func C(x int) float64 {
 // todo: test this
 func DCT(F *[][]float64) [][]float64 {
 	var result *Block = createEmptyBlock("")
-	for u := range 8 {
-		for v := range 8 {
+	for u := 0; u < 8; u++ {
+		for v := 0; v < 8; v++ {
 			var sum float64 = 0
-			for x := range 8 {
-				for y := range 8 {
-					sum += math.Cos((float64(2*x+1)*float64(u)*math.Pi)/(2*8)) * math.Cos((float64(2*y+1)*float64(v)*math.Pi)/(2*8)) * (*F)[x][y]
+			for x := 0; x < 8; x++ {
+				for y := 0; y < 8; y++ {
+					sum += math.Cos((float64((2*x+1)*u)*math.Pi)/(2*8)) * math.Cos((float64((2*y+1)*v)*math.Pi)/(2*8)) * (*F)[x][y]
 				}
 			}
-			sum *= C(u) * C(v) * 0.25 // this is usually (2 / math.Sqrt(n * m)) but its always 8x8 blocks
+			sum *= C(u) * C(v) * (2 / math.Sqrt(8*8)) // this is usually (2 / math.Sqrt(n * m)) but its always 8x8 blocks
 			result.Matrix[u][v] = sum
 		}
 	}
@@ -40,7 +42,7 @@ func IDCT(H [][]float64) [][]float64 {
 			var sum float64 = 0
 			for u := range 8 {
 				for v := range 8 {
-					sum += 2 * ((C(u) * C(v)) / 8) * math.Cos((float64(2*x+1)*float64(u)*math.Pi)/(2*8)) * math.Cos((float64(2*y+1)*float64(v)*math.Pi)/float64(2*8)) * H[u][v]
+					sum += 2 * ((C(u) * C(v)) / 8) * math.Cos((float64((2*x+1)*u)*math.Pi)/(2*8)) * math.Cos((float64((2*y+1)*v)*math.Pi)/(2*8)) * H[u][v]
 				}
 			}
 			result[x][y] = sum
